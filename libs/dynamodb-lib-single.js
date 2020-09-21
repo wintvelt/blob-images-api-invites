c6d0c3eac2e8d72d20bc6865fbc7574333cf0bc5
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-09-22 00:03:20 +0200

    clean up libs

--- libs/dynamodb-lib-single.js
+++ libs/dynamodb-lib-single.js
@@ -1,6 +1,4 @@
 import { dynamoDb } from 'blob-common/core/db';
-import { listPhotoPublications } from './dynamodb-lib-photo';
-import { getMemberships } from './dynamodb-lib-memberships';
 
 export const getMember = async (userId, groupId) => {
     const memberParams = {
@@ -12,48 +10,4 @@ export const getMember = async (userId, groupId) => {
     };
     const result = await dynamoDb.get(memberParams);
     return (result.Item);
-};
-export const getMemberRole = async (userId, groupId) => {
-    const membership = await getMember(userId, groupId);
-    return membership && (membership.status !== 'invite') && membership.role;
-};
-export const checkUser = async (userId, groupId) => {
-    const groupRole = await getMemberRole(userId, groupId);
-    return (!!groupRole);
-};
-
-export const getPhotoByUser = async (photoId, userId) => {
-    const params = {
-        TableName: process.env.photoTable,
-        Key: {
-            PK: 'PO' + photoId,
-            SK: userId,
-        }
-    };
-
-    const result = await dynamoDb.get(params);
-    if (!result.Item) {
-        throw new Error("Photo not found.");
-    }
-
-    // Return the retrieved item
-    return result.Item;
-};
-
-const getGroupId = (key) => key.split('#')[0].slice(2);
-
-export const getPhotoById = async (photoId, userId) => {
-    // returns photo if user has any access
-    try {
-        const photo = await getPhotoByUser(photoId, userId);
-        return photo;
-    } catch (_) {
-        // get all publications and return if user is member of any
-        const publications = await listPhotoPublications(photoId);
-        const memberships = await getMemberships(userId);
-        const groupsWithUser = memberships.map(mem => mem.SK);
-        const pubsWithMembership = publications.filter(pub => groupsWithUser.includes(getGroupId(pub.PK)));
-        if (pubsWithMembership.length === 0) return undefined;
-        return pubsWithMembership[0].photo;
-    }
 };
