816a7c6d3910a71fea22082456ec157c08ce7941
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-10-02 10:18:37 +0200

    finish env stage var

--- serverlessDomainConfig.js
+++ serverlessDomainConfig.js
@@ -7,3 +7,8 @@ module.exports.photoTable = () => ({
     'dev': 'blob-images-photos-dev',
     'prod': 'blob-images-photos'
 });
+
+module.exports.frontend = () => ({
+    'dev': 'http://localhost:3000',
+    'prod': 'clubalmanac.com'
+});
